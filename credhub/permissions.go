a7e4af5494ed525b884e7bb87174384e1d4ab5eb
Victoria Henry <vhenry@pivotal.io>
2018-10-01 17:06:33 -0400

    still working on get/add permissions tests; get request failing

--- credhub/permissions.go
+++ credhub/permissions.go
@@ -4,10 +4,12 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 
 	"encoding/json"
 
 	"code.cloudfoundry.org/credhub-cli/credhub/permissions"
+	"github.com/hashicorp/go-version"
 )
 
 type permissionsResponse struct {
@@ -15,7 +17,27 @@ type permissionsResponse struct {
 	Permissions    []permissions.Permission `json:"permissions"`
 }
 
-func (ch *CredHub) GetPermission(uuid string) (*permissions.Permission, error) {
+func (ch *CredHub) getV1Permission(name string) ([]permissions.Permission, error) {
+	query := url.Values{}
+	query.Set("credential_name", name)
+
+	resp, err := ch.Request(http.MethodGet, "/api/v1/permissions", query, nil, true)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	defer io.Copy(ioutil.Discard, resp.Body)
+	var response permissionsResponse
+
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, err
+	}
+
+	return response.Permissions, nil
+}
+
+func (ch *CredHub) getV2Permission(uuid string) (*permissions.Permission, error) {
 	path := "/api/v2/permissions/" + uuid
 
 	resp, err := ch.Request(http.MethodGet, path, nil, nil, true)
@@ -33,6 +55,29 @@ func (ch *CredHub) GetPermission(uuid string) (*permissions.Permission, error) {
 	}
 
 	return &response, nil
+}
+
+func (ch *CredHub) GetPermission(param string) (*permissions.Permission, error) {
+	if ch.cachedServerVersion == "" {
+		serverVersion, err := ch.ServerVersion()
+		if err != nil {
+			return nil, err
+		}
+		ch.cachedServerVersion = serverVersion.String()
+	}
+
+	serverVersion, err := version.NewVersion(ch.cachedServerVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	if serverVersion.Segments()[0] < 2 {
+		ch.getV1Permission(param)
+	} else {
+		ch.getV2Permission(param)
+	}
+
+	return nil, nil
 }
 
 func (ch *CredHub) AddV1Permissions(credName string, perms []permissions.Permission) ([]permissions.Permission, error) {
@@ -71,5 +116,24 @@ func (ch *CredHub) AddV2Permission(path string, actor string, ops []string) (*pe
 }
 
 func (ch *CredHub) AddPermission(path string, actor string, ops []string) (*permissions.Permission, error) {
+	if ch.cachedServerVersion == "" {
+		serverVersion, err := ch.ServerVersion()
+		if err != nil {
+			return nil, err
+		}
+		ch.cachedServerVersion = serverVersion.String()
+	}
+
+	serverVersion, err := version.NewVersion(ch.cachedServerVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	if serverVersion.Segments()[0] < 2 {
+		ch.AddV1Permissions(path, []permissions.Permission{{Actor: actor, Operations: ops}})
+	} else {
+		ch.AddV2Permission(path, actor, ops)
+	}
+
 	return nil, nil
 }
--- credhub/permissions_test.go
+++ credhub/permissions_test.go
@@ -4,137 +4,124 @@ import (
 	. "code.cloudfoundry.org/credhub-cli/credhub"
 	"github.com/onsi/gomega/ghttp"
 
-	"bytes"
-	"io/ioutil"
 	"net/http"
 
-	"code.cloudfoundry.org/credhub-cli/credhub/permissions"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Permissions", func() {
+var _ = FDescribe("Permissions", func() {
+//	Context("GetPermissions", func() {
+//		It("returns the permissions", func() {
+//			responseString :=
+//				`{
+//	"actor":"user:A",
+//	"operations":["read"],
+//	"path":"/example-password",
+//	"uuid":"1234"
+//}`
+//
+//			dummyAuth := &DummyAuth{Response: &http.Response{
+//				StatusCode: http.StatusOK,
+//				Body:       ioutil.NopCloser(bytes.NewBufferString(responseString)),
+//			}}
+//
+//			ch, _ := New("https://example.com", Auth(dummyAuth.Builder()))
+//			actualPermissions, err := ch.GetPermission("1234")
+//			Expect(err).NotTo(HaveOccurred())
+//
+//			expectedPermission := permissions.Permission{
+//				Actor:      "user:A",
+//				Operations: []string{"read"},
+//				Path:       "/example-password",
+//				UUID:       "1234",
+//			}
+//			Expect(actualPermissions).To(Equal(&expectedPermission))
+//
+//			By("calling the right endpoints")
+//			url := dummyAuth.Request.URL.String()
+//			Expect(url).To(Equal("https://example.com/api/v2/permissions/1234"))
+//			Expect(dummyAuth.Request.Method).To(Equal(http.MethodGet))
+//		})
+//	})
+
 	Context("GetPermissions", func() {
-		It("returns the permissions", func() {
-			responseString :=
-				`{
-	"actor":"user:A",
-	"operations":["read"],
-	"path":"/example-password",
-	"uuid":"1234"
-}`
+		Context("when server version is less than 2.0.0", func() {
+			Context("when permission exists", func() {
+				var server *ghttp.Server
 
-			dummyAuth := &DummyAuth{Response: &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       ioutil.NopCloser(bytes.NewBufferString(responseString)),
-			}}
-
-			ch, _ := New("https://example.com", Auth(dummyAuth.Builder()))
-			actualPermissions, err := ch.GetPermission("1234")
-			Expect(err).NotTo(HaveOccurred())
-
-			expectedPermission := permissions.Permission{
-				Actor:      "user:A",
-				Operations: []string{"read"},
-				Path:       "/example-password",
-				UUID:       "1234",
-			}
-			Expect(actualPermissions).To(Equal(&expectedPermission))
-
-			By("calling the right endpoints")
-			url := dummyAuth.Request.URL.String()
-			Expect(url).To(Equal("https://example.com/api/v2/permissions/1234"))
-			Expect(dummyAuth.Request.Method).To(Equal(http.MethodGet))
-		})
-	})
-
-	Context("AddV2Permissions", func() {
-		Context("when a credential exists", func() {
-			It("can add permissions to it", func() {
-				responseString :=
-					`{
-	"actor":"user:B",
-	"operations":["read"],
-	"path":"/example-password"
-}`
-				dummy := &DummyAuth{Response: &http.Response{
-					StatusCode: http.StatusCreated,
-					Body:       ioutil.NopCloser(bytes.NewBufferString(responseString)),
-				}}
-				ch, _ := New("https://example.com", Auth(dummy.Builder()))
-
-				_, err := ch.AddV2Permission("/example-password", "user:A", []string{"read_acl", "write_acl"})
-				Expect(err).NotTo(HaveOccurred())
-
-				By("calling the right endpoints")
-				url := dummy.Request.URL.String()
-				Expect(url).To(Equal("https://example.com/api/v2/permissions"))
-				Expect(dummy.Request.Method).To(Equal(http.MethodPost))
-				params, err := ioutil.ReadAll(dummy.Request.Body)
-				Expect(err).NotTo(HaveOccurred())
-
-				expectedParams := `{
-				"actor": "user:A",
-				"operations": ["read_acl", "write_acl"],
-				"path": "/example-password"
-			}`
-				Expect(params).To(MatchJSON(expectedParams))
-			})
-		})
-	})
-
-	Context("AddV1Permissions", func() {
-		Context("when a credential exists", func() {
-			var server *ghttp.Server
-
-			BeforeEach(func() {
-				server = ghttp.NewServer()
-				server.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("POST", "/api/v1/permissions"),
-						ghttp.RespondWith(http.StatusOK, `{
-	"credential_name": "/example-password",
-	"permissions": [
-		{
-	  		"actor": "uaa-user:e3366b5c-1c5a-4df8-8b0f-9001ee5a0cf0",
-	  		"operations": ["read"]
-		}
-	]
-}`),
-					),
-				)
-			})
-
-			AfterEach(func() {
-				//shut down the server between tests
-				server.Close()
-			})
-
-			It("can add permissions to it", func() {
-
-				ch, _ := New(server.URL(), ServerVersion("1.9.0"))
-				_, err := ch.AddV1Permissions("/example-password", []permissions.Permission{
-					{
-						Actor:      "uaa-user:e3366b5c-1c5a-4df8-8b0f-9001ee5a0cf0",
-						Operations: []string{"read"},
-						Path:       "/example-password",
-					},
+				BeforeEach(func() {
+					server = ghttp.NewServer()
+					server.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/info"),
+							ghttp.RespondWith(http.StatusOK, `{}`),
+						),
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/version"),
+							ghttp.RespondWith(http.StatusOK, `{"version": "1.9.0"}`),
+						),
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/api/v1/permissions?credential_name=/example-password"),
+							ghttp.RespondWith(http.StatusOK, `{}`),
+						),
+					)
 				})
-				Expect(err).NotTo(HaveOccurred())
+
+				AfterEach(func() {
+					//shut down the server between tests
+					server.Close()
+				})
+
+				It("returns permission", func() {
+					ch, _ := New(server.URL())
+					_, err := ch.GetPermission("name")
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).To(HaveLen(3))
+				})
+			})
+		})
+
+		Context("when the server version is greater than or equal to 2.0.0", func() {
+			Context("when permissions exists", func() {
+				var server *ghttp.Server
+
+				BeforeEach(func() {
+					server = ghttp.NewServer()
+					server.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/info"),
+							ghttp.RespondWith(http.StatusOK, `{}`),
+						),
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/version"),
+							ghttp.RespondWith(http.StatusOK, `{"version": "2.0.0"}`),
+						),
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/api/v2/permissions/1234"),
+							ghttp.RespondWith(http.StatusOK, `{}`),
+						),
+					)
+				})
+
+				AfterEach(func() {
+					//shut down the server between tests
+					server.Close()
+				})
+
+				It("returns permission", func() {
+					ch, _ := New(server.URL())
+					_, err := ch.GetPermission("1234")
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).To(HaveLen(3))
+				})
 			})
 		})
 	})
 
-	Context("AddPermission", func() {
-		Context("when a credential exists", func() {
-			var server *ghttp.Server
-
-			AfterEach(func() {
-				//shut down the server between tests
-				server.Close()
-			})
-
-			Context("When the server version is not provided", func() {
+	Context("AddPermissions", func() {
+		Context("when server version is less than 2.0.0", func() {
+			Context("when a credential exists", func() {
 				var server *ghttp.Server
 
 				BeforeEach(func() {
@@ -150,29 +137,102 @@ var _ = Describe("Permissions", func() {
 						),
 						ghttp.CombineHandlers(
 							ghttp.VerifyRequest("POST", "/api/v1/permissions"),
+							ghttp.VerifyJSON(`{
+	"credential_name": "/example-password",
+	"permissions": [
+	{
+	"actor": "some-actor",
+	"operations": ["read", "write"],
+	"path":"", "uuid":""
+	}]
+	}`),
 							ghttp.RespondWith(http.StatusOK, `{}`),
 						),
 					)
 				})
 
-				It("sends a request for the server version", func() {
+				AfterEach(func() {
+					//shut down the server between tests
+					server.Close()
+				})
+
+				It("can add permissions to it", func() {
 					ch, _ := New(server.URL())
-					_, err := ch.AddPermission("/example-certificate", "user:A", []string{"operation1","operation2"})
+					_, err := ch.AddPermission("/example-password", "some-actor", []string{"read", "write"})
 					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).To(HaveLen(3))
 				})
 			})
+
+			//Context("when a credential doesn't exist", func() {
+			//	It("cannot add permissions to it", func() {
+			//		dummy := &DummyAuth{Response: &http.Response{
+			//			StatusCode: http.StatusNotFound,
+			//			Body:       ioutil.NopCloser(bytes.NewBufferString(`{"error":"The request could not be completed because the credential does not exist or you do not have sufficient authorization."}`)),
+			//		}}
+			//		ch, _ := New("https://example.com", Auth(dummy.Builder()))
+			//
+			//		_, err := ch.AddPermission("/example-password", "some-actor", []string{"read", "write"})
+			//
+			//		Expect(err).To(MatchError(ContainSubstring("The request could not be completed because the credential does not exist or you do not have sufficient authorization.")))
+			//	})
+			//})
 		})
 
-		Context("when the server version is older than 2.x", func() {
-			It("sends the v1 model request", func(){
-				dummy := &DummyAuth{Response: &http.Response{
-					Body: ioutil.NopCloser(bytes.NewBufferString("")),
-				}}
+		Context("when the server version is greater than or equal to 2.0.0", func() {
+			Context("when a credential exists", func() {
+				var server *ghttp.Server
+
+				BeforeEach(func() {
+					server = ghttp.NewServer()
+					server.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/info"),
+							ghttp.RespondWith(http.StatusOK, `{}`),
+						),
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/version"),
+							ghttp.RespondWith(http.StatusOK, `{"version": "2.0.0"}`),
+						),
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("POST", "/api/v2/permissions"),
+							ghttp.VerifyJSON(`{
+	"actor": "some-actor",
+	"operations":["read", "write"],
+	"path":"/example-password"
+	}`),
+							ghttp.RespondWith(http.StatusOK, `{}`),
+						),
+					)
+				})
+
+				AfterEach(func() {
+					//shut down the server between tests
+					server.Close()
+				})
+
+				It("can add permissions to it", func() {
+					ch, _ := New(server.URL())
+					_, err := ch.AddPermission("/example-password", "some-actor", []string{"read", "write"})
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).To(HaveLen(3))
+				})
 			})
-		})
 
-		Context("when the server version is 2.x or later", func() {
-
+			//Context("when a credential doesn't exist", func() {
+			//	It("cannot add permissions to it", func() {
+			//		dummy := &DummyAuth{Response: &http.Response{
+			//			StatusCode: http.StatusNotFound,
+			//			Body:       ioutil.NopCloser(bytes.NewBufferString(`{"error":"The request could not be completed because the credential does not exist or you do not have sufficient authorization."}`)),
+			//		}}
+			//		ch, _ := New("https://example.com", Auth(dummy.Builder()))
+			//
+			//		_, err := ch.AddPermission("/example-password", "some-actor", []string{"read", "write"})
+			//
+			//		Expect(err).To(MatchError(ContainSubstring("The request could not be completed because the credential does not exist or you do not have sufficient authorization.")))
+			//	})
+			//})
 		})
 	})
 })
+
